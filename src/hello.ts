78586c6b010772b26cda6d6e16598d8b8c26f9cc
Neal Fennimore <hi@neal.codes>
2020-03-23 18:17:48 -0400

    Update hello

--- src/hello.ts
+++ src/hello.ts
@@ -1,15 +1,40 @@
 import { APIGatewayEvent } from 'aws-lambda';
+import { ApolloServer, gql } from  'apollo-server-lambda';
 
-export async function handler( event: APIGatewayEvent ): Promise<any> {
-    return {
-        statusCode: 200,
-        body: JSON.stringify(
-            {
-                message: 'Hello Serverless v1.0! Your function executed successfully!',
-                input: event,
-            },
-            null,
-            2
-        ),
-    };
-}
+const typeDefs = gql`
+    type Response {
+        statusCode: Int
+        body: String
+    }
+
+    type Query {
+        hello: Response
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello: ( event: APIGatewayEvent ): Record<string, any> => {
+            return {
+                statusCode: 200,
+                body: JSON.stringify(
+                    {
+                        message: 'Hello Serverless v1.0! Your function executed successfully!',
+                        input: event,
+                    },
+                    null,
+                    2
+                ),
+            };
+        }
+    },
+};
+
+const server = new ApolloServer( { typeDefs, resolvers } );
+
+export const handler = server.createHandler( {
+    cors: {
+        origin: '*',
+        credentials: true,
+    },
+} );
