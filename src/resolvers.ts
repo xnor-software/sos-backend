8293a49fc8d5f514b27b741239a509e8412afc2b
Neal Fennimore <hi@neal.codes>
2020-03-24 15:30:38 -0400

    Fix linter

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,19 +1,18 @@
 import { APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda';
 
+// eslint-disable-next-line import/prefer-default-export
 export const resolvers = {
     Query: {
-        hello: (event: APIGatewayEvent): APIGatewayProxyResult => {
-            return {
-                statusCode: 200,
-                body: JSON.stringify(
-                    {
-                        message: 'Hello Serverless v1.0! Your function executed successfully!',
-                        input: event,
-                    },
-                    null,
-                    2
-                ),
-            };
-        }
+        hello: ( event: APIGatewayEvent ): APIGatewayProxyResult => ( {
+            statusCode: 200,
+            body: JSON.stringify(
+                {
+                    message: 'Hello Serverless v1.0! Your function executed successfully!',
+                    input: event,
+                },
+                null,
+                2,
+            ),
+        } ),
     },
 };
--- src/schema.ts
+++ src/schema.ts
@@ -1,5 +1,6 @@
 import { gql } from 'apollo-server-lambda';
 
+// eslint-disable-next-line import/prefer-default-export
 export const schema = gql`
     type APIGatewayProxyResult {
         statusCode: Int
